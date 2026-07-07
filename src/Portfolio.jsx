409f0fb167db9c357a9bd3660dd8a4cd5563c339
ParulGautam27 <parulgautam9813@gmail.com>
2026-07-07 21:19:51 +0530

    Update Files

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Portfolio.css";
 import shopease from './assets/shopEase.png'
 import stayease from './assets/stayease.png'
+import ecoscale from '../assets/ecoscale-preview.png';
 import phishingDetector from "./assets/phishingDetector.png";
 import aijarvis from './assets/aijarvis.png'
 import photoGallery from './assets/photoGallery.png'
